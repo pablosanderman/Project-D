d4405c5375c801288db2f47be4c668670e9073fe
Korbin de Man <korbindeman4@gmail.com>
2024-03-26 13:57:45 +0100

    using database in server

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,15 @@
 import { createHTTPServer } from "@trpc/server/adapters/standalone";
 import { publicProcedure, router } from "./trpc";
+import prisma from "@/utils/prisma";
 
 const appRouter = router({
   helloWorld: publicProcedure.query(async () => {
-    return { text: "Hello world!" };
+    const user = await prisma.user.findUnique({
+      where: {
+        id: 1,
+      },
+    });
+    return { text: "Hello " + user?.name };
   }),
 });
 
