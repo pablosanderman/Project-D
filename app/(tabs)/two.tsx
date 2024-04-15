43ecbe3259155e4625b95b9b7bb142ad8215760d
schtormm <storm.noordermeer@gmail.com>
2024-04-15 21:46:52 +0200

    feat: displaying customer bookings backend (with basic frontend)

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,29 +1,63 @@
-import { StyleSheet } from 'react-native';
+import { Button, FlatList, StyleSheet } from "react-native";
 
-import { Text, View } from '@/components/Themed';
+import { Text, View } from "@/components/Themed";
+import { trpc } from "@/utils/trpc";
 
 export default function TabTwoScreen() {
+  const query = trpc.booking.get.useQuery({ userId: 1 });
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Text style={styles.separator}></Text>
+      <Text style={styles.title}>Your booking</Text>
+      {query.data && (
+        <View>
+          <FlatList
+            data={query.data}
+            renderItem={({ item }) => (
+              <View style={styles.container}>
+                <Text>Booking no. {item.id}</Text>
+                <Text>Kamer {item.roomId}</Text>
+                <Text>Start: {formatDate(item.startTime)}</Text>
+                <Text>Eindigt: {formatDate(item.endTime)}</Text>
+                <Text>Booking status: {item.status}</Text>
+                <View style={styles.separator}></View>
+              </View>
+            )}
+          />
+        </View>
+      )}
+      {query.error && <Text>Something went wrong! {query.error.message}</Text>}
     </View>
   );
 }
 
+const formatDate = (dateString: string) => {
+  const date = new Date(dateString);
+
+  const formattedDate = date.toLocaleString("nl-NL", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  });
+  return formattedDate;
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
