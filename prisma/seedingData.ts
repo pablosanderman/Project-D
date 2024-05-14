b1fb843391075d4e83e95916f2ec06245cf4d1f0
Pablo Sanderman <pablosanderman@gmail.com>
2024-05-14 12:19:06 +0200

    * Added NavigationId field * Added seeding for the rooms of floor 2

--- prisma/seedingData.ts
+++ prisma/seedingData.ts
@@ -7,7 +7,7 @@
 
 // enum RoomType {
 //   MEETING
-//   CONCENTRATION
+//   FOCUS
 //   DESK
 // }
 
@@ -36,13 +36,16 @@
 // model Room {
 //   id        Int       @id @default(autoincrement())
 //   name      String
+//   type      RoomType
+//   capacity  Int
+//   floor     Int
+//   navigationId String
 //   bookings  Booking[]
 //   createdAt DateTime  @default(now())
 //   updatedAt DateTime  @updatedAt
-//   type      RoomType
 // }
 
-import { BookingStatus, RoomType, RoomSize } from "@prisma/client";
+import { BookingStatus, RoomType } from "@prisma/client";
 
 const bookings = [
   {
@@ -83,26 +86,118 @@ const users = [
   },
 ];
 
-const rooms = [];
+const rooms = [
+  {
+    name: "PL 02.01",
+    type: RoomType.MEETING,
+    capacity: 8,
+    floor: 2,
+    navigationId: "02.01",
+  },
+  {
+    name: "PL 02.02",
+    type: RoomType.MEETING,
+    capacity: 8,
+    floor: 2,
+    navigationId: "02.02",
+  },
+  {
+    name: "PL 02.03",
+    type: RoomType.MEETING,
+    capacity: 12,
+    floor: 2,
+    navigationId: "02.03",
+  },
+  {
+    name: "PL 02.04",
+    type: RoomType.MEETING,
+    capacity: 12,
+    floor: 2,
+    navigationId: "02.04",
+  },
+  {
+    name: "PL 02.05",
+    type: RoomType.MEETING,
+    capacity: 12,
+    floor: 2,
+    navigationId: "02.05",
+  },
+  {
+    name: "PL 02.06",
+    type: RoomType.MEETING,
+    capacity: 12,
+    floor: 2,
+    navigationId: "02.06",
+  },
+  {
+    name: "PL 02.21",
+    type: RoomType.FOCUS,
+    capacity: 2,
+    floor: 2,
+    navigationId: "02.21",
+  },
+  {
+    name: "PL 02.22",
+    type: RoomType.FOCUS,
+    capacity: 1,
+    floor: 2,
+    navigationId: "02.22",
+  },
+  {
+    name: "PL 02.23",
+    type: RoomType.FOCUS,
+    capacity: 1,
+    floor: 2,
+    navigationId: "02.23",
+  },
+  {
+    name: "PL 02.29",
+    type: RoomType.MEETING,
+    capacity: 6,
+    floor: 2,
+    navigationId: "02.29",
+  },
+  {
+    name: "PL 02.36",
+    type: RoomType.MEETING,
+    capacity: 6,
+    floor: 2,
+    navigationId: "02.36",
+  },
+  {
+    name: "PL 02.37",
+    type: RoomType.FOCUS,
+    capacity: 1,
+    floor: 2,
+    navigationId: "02.37",
+  },
+  {
+    name: "PL 02.38",
+    type: RoomType.FOCUS,
+    capacity: 1,
+    floor: 2,
+    navigationId: "02.38",
+  },
+  ...createDeskCluster(2, 18, "02.C1"),
+  ...createDeskCluster(2, 24, "02.C2"),
+  ...createDeskCluster(2, 12, "02.C3"),
+  ...createDeskCluster(2, 17, "02.C4"),
+  ...createDeskCluster(2, 8, "02.C5"),
+  ...createDeskCluster(2, 24, "02.C6"),
+  ...createDeskCluster(2, 6, "02.C7"),
+  ...createDeskCluster(2, 18, "02.C8"),
+  ...createDeskCluster(2, 6, "02.C9"),
+  ...createDeskCluster(2, 20, "02.C10"),
+];
 
-for (let i = 1; i <= 100; i++) {
-  rooms.push({
-    name: `Room ${i}`,
-    type:
-      i % 3 === 0
-        ? RoomType.DESK
-        : i % 2 === 0
-        ? RoomType.FOCUS
-        : RoomType.MEETING,
-    size:
-      i % 4 === 0
-        ? RoomSize.EIGHT_TO_SIXTEEN
-        : i % 3 === 0
-        ? RoomSize.FOUR_TO_EIGHT
-        : i % 2 === 0
-        ? RoomSize.TWO_TO_FOUR
-        : RoomSize.ONE_TO_TWO,
-  });
+function createDeskCluster(floor: number, count: number, navigationId: string) {
+  return Array.from({ length: count }, (_, index) => ({
+    name: `PL ${floor}.D${index + 1}`,
+    type: RoomType.DESK,
+    capacity: 1,
+    floor,
+    navigationId: navigationId,
+  }));
 }
 
 export const seedingData = {
